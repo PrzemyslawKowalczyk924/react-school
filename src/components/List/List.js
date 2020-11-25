416309a74fb34ca75ebb5bc0371e8ad09091475f
Przemysław Kowalczyk <przemek.t.kowalczyk@gmail.com>
2020-11-25 23:44:35 +0100

    Install htmlparser & parse title in Hero.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,27 +3,29 @@ import Hero from '../Hero/Hero.js';
 import styles from './List.scss';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
+    description: PropTypes.node,
+    columns: PropTypes.array,
     image: PropTypes.string,
   };
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   };
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} image={this.props.image} />
         <div className={styles.description}>
-            {this.props.children}
+          {this.props.description}
         </div>
         <div className={styles.columns}>
-          <Column className={styles.title} title={'Column'}/>
-          <Column className={styles.title} title={'Columns'}/>
-          <Column className={styles.title} title={'Columner'}/>
+          <Column className={styles.title} title={'Praca'}/>
+          <Column className={styles.title} title={'Rodzina'}/>
+          <Column className={styles.title} title={'Nauka'}/>
         </div>
       </section>
     );
