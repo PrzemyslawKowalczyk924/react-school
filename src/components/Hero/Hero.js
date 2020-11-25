d76ab397af461a2064dd824666cd052080a6a5e5
Przemysław Kowalczyk <przemek.t.kowalczyk@gmail.com>
2020-11-25 18:18:13 +0100

    Add prop-type for Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{props.titleText}</h2>
     {/*<img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"/>*/}
-    <img className={styles.image} src={this.props.image}/>
+    <img className={styles.image} src={props.image}/>
   </header>
 );
 
