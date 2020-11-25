16ca79376276046aee887c2a1300437cf284942a
Przemysław Kowalczyk <przemek.t.kowalczyk@gmail.com>
2020-11-25 23:49:09 +0100

    Add htmlParser to description in List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,6 +4,7 @@ import styles from './List.scss';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
@@ -20,7 +21,7 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} image={this.props.image} />
         <div className={styles.description}>
-          {this.props.description}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
           <Column className={styles.title} title={'Praca'}/>
