4a1d7bc2b1825a83c8fa980bf7ed5f721efa606a
Przemysław Kowalczyk <przemek.t.kowalczyk@gmail.com>
2020-12-04 23:29:58 +0100

    Add list component to redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-//import List from '../List/List';
+import List from '../List/List';
 import styles from './App.scss';
 import PropTypes from 'prop-types';
-
 
 class App extends React.Component {
   static propTypes = {
     title: PropTypes.node,
     subtitle: PropTypes.node,
+    lists: PropTypes.array,
   }
   render() {
-    const {title, subtitle} = this.props;
+    const {title, subtitle, lists} = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        {/*
-        <List {...listData} />
-        */}
+        {lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))};
       </main>
     );
   }
