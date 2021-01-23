8de90cc17c02fdb7ae88f310f02196e22e5fda97
unknown <kowalczyk.przemek@wp.pl>
2021-01-23 01:48:26 +0100

    Add card reducer

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -17,7 +17,7 @@ class Column extends React.Component {
     icon: settings.defaultColumnIcon,
   };
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -32,7 +32,7 @@ class Column extends React.Component {
           ))}
         </div>
         <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+          <Creator text={settings.cardCreatorText} action={addCard} />
         </div>
       </section>
     );
