c0b26e1457fcf87780a775eef0b15d9481f3e4e1
unknown <kowalczyk.przemek@wp.pl>
2021-01-26 23:50:50 +0100

    install transition for react

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Container from '../Container/Container';
 import Hero from '../Hero/Hero';
 import {faqContent} from '../../data/dataStore';
+import grayscale from './FAQ.scss';
 
 const FAQ = () => (
   <Container>
-    <Hero titleText={faqContent.title} image={faqContent.image}/>
+    <Hero titleText={faqContent.title} image={faqContent.image} className={grayscale} />
     <h2>{faqContent.header}</h2>
     <p>{faqContent.description}</p>
   </Container>
